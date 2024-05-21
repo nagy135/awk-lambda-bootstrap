ea7ae6bd9ab148f0fce08a6cc9f0f6e71fc54014
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-05-21 23:57:23 +0200

    refactor: health lambda

--- lambda/health.ts
+++ lambda/health.ts
@@ -1,16 +1,12 @@
 import { APIGatewayEvent } from "aws-lambda";
 
-const handler = async function(event: APIGatewayEvent) {
+export const handler = async function(event: APIGatewayEvent) {
 
 	console.log("request:", JSON.stringify(event, undefined, 2));
 
 	return {
 		statusCode: 200,
 		headers: { "Content-Type": "text/plain" },
-		body: `OK, ${event.body}\n`
+		body: `OK`
 	};
-};
-
-export {
-	handler
 };
